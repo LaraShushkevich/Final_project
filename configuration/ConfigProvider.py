e75a820bb7adaae6738357a85c84b74c8b289827
LaraShushkevich <you@example.com>
2026-01-30 19:41:50 +0300

    commit 8

---
+++ configuration/ConfigProvider.py
@@ -0,0 +1,22 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read('test_config.ini')
+
+
+class ConfigProvider:
+
+    def __init__(self) -> None:
+        self.config = config
+
+    def get(self, section: str, prop: str) -> str:
+        return self.config[section].get(prop)
+
+    def getint(self, section: str, prop: str) -> str:
+        return self.config[section].getint(prop)
+
+    def get_ui_url(self) -> str:
+        return self.config["ui"].get("base_url")
+
+    def get_api_url(self) -> str:
+        return self.config["api"].get("base_url")
